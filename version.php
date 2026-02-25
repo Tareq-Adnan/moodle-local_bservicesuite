b38e4f2ee16102f33e9f4b2ae6289df5687ac288
Tareq-Adnan <adnantareq2@gmail.com>
2026-02-25 13:25:14 +0600

    version update

--- version.php
+++ version.php
@@ -27,7 +27,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component    = 'local_bservicesuite';
 $plugin->release      = '1.0';
-$plugin->version      = 2026022700;
+$plugin->version      = 2026022710;
 $plugin->requires     = 2024100700;
 $plugin->supported    = [405, 501];
 $plugin->maturity     = MATURITY_STABLE;
